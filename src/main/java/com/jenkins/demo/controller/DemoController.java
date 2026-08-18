f1f5a61a2160e6fe49c7867bf92ab26a92be5005
Sriramdev1604 <sriramvenkatesandev@gmail.com>
2026-08-18 15:43:11 +0530

    jenkins-tut

---
+++ src/main/java/com/jenkins/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello this is java spring boot application done by jenkins session";
+    }
+}
